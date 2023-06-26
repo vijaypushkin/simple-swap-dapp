4fd0658cd70505013fdb2f4af236f7336c584ecb
Vijay Pushkin <vijaypushkin.a@gmail.com>
2023-06-27 01:07:35 +0530

    feat: swap feature added

--- src/components/swap/swap.tsx
+++ src/components/swap/swap.tsx
@@ -1,41 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useWalletContext } from '../../context/context-hooks.ts';
+
+const RATIO = 0.9975;
 
 const Swap: React.FC = () => {
-  return (
-    <div className="flex flex-col gap-2 border rounded-xl mx-auto max-w-lg p-4">
-      <h1 className="text-2xl">Swap</h1>
+  const { account, handleConnectWallet } = useWalletContext();
+
+  const [firstToken, setFirstToken] = useState('');
+
+  const [secondToken, setSecondToken] = useState('');
+
+  const [inverse, setInverse] = useState(false);
+
+  const handleFirstTokenChange = (
+    ev: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    // ? empty input
+    if (ev.target.value === '') {
+      setSecondToken('');
+      setFirstToken('');
+      return void 0;
+    }
+
+    // ? not a number
+    if (isNaN(parseFloat(ev.target.value))) return void 0;
+
+    setFirstToken(ev.target.value);
+    setSecondToken((parseFloat(ev.target.value) * RATIO).toString());
+  };
+
+  const handleSecondTokenChange = (
+    ev: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    // ? empty input
+    if (ev.target.value === '') {
+      setSecondToken('');
+      setFirstToken('');
+      return void 0;
+    }
+
+    // ? not a number
+    if (isNaN(parseFloat(ev.target.value))) return void 0;
+
+    setSecondToken(ev.target.value);
+    setFirstToken((parseFloat(ev.target.value) / RATIO).toString());
+  };
+
+  const handleSwap = () => {
+    if (!account) {
+      handleConnectWallet();
+      return void 0;
+    }
+
+    setTimeout(() => {
+      alert('Swap successful!');
+    }, 3000);
+  };
+
+  const renderToken = (name: 'first' | 'second') => {
+    const value = name === 'first' ? firstToken : secondToken;
+    const onChange =
+      name === 'first' ? handleFirstTokenChange : handleSecondTokenChange;
+    const tokenName = name === 'first' ? 'MATIC' : 'USDC';
+
+    const valueInUSD = isNaN(parseFloat(value)) ? 0 : parseFloat(value) * 0.75;
+
+    return (
       <div className="flex flex-col border rounded-xl p-4 gap-2 bg-gray-800">
         <div className="flex flex-row">
           <input
-            className="grow text-2xl bg-transparent"
+            className="grow text-lg md:text-2xl bg-transparent"
             type="text"
             pattern="^[0-9]*[.,]?[0-9]*$"
+            value={value}
+            onChange={onChange}
           />
-          <button className="token px-2 py-1">MATIC</button>
+          <button className="token px-2 py-1">{tokenName}</button>
         </div>
 
-        <div className="text-xs">$10</div>
+        <div className="text-xs">${valueInUSD}</div>
       </div>
+    );
+  };
 
-      <button className="-my-4 border p-2 mx-auto rounded-xl active:scale-[99%]">
-        s
+  return (
+    <div className="flex flex-col gap-2 border rounded-xl mx-auto max-w-lg p-4 m-2">
+      <h1 className="text-2xl">Swap</h1>
+
+      {renderToken(inverse ? 'second' : 'first')}
+
+      <button
+        className="-my-4 border p-2 mx-auto rounded-xl active:scale-[99%] bg-gray-900 z-10"
+        onClick={() => setInverse((s) => !s)}
+      >
+        ⇕
       </button>
 
-      <div className="flex flex-col border rounded-xl p-4 gap-2 bg-gray-800">
-        <div className="flex flex-row">
-          <input
-            className="grow text-2xl bg-transparent"
-            type="text"
-            pattern="^[0-9]*[.,]?[0-9]*$"
-          />
-          <button className="token px-2 py-1">MATIC</button>
-        </div>
+      {renderToken(inverse ? 'first' : 'second')}
 
-        <div className="text-xs">$10</div>
-      </div>
-
-      <button className="bg-blue-900 h-12 rounded-xl mt-2 active:scale-[99%]">
-        Swap
+      <button
+        className="bg-blue-900 h-12 rounded-xl mt-2 active:scale-[99%]"
+        onClick={handleSwap}
+      >
+        {account ? 'Swap' : 'Connect Wallet'}
       </button>
     </div>
   );
