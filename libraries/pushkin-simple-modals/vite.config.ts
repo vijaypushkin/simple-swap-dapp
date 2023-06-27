1a22896f552a55cd0d5cd61a646878d0455f0b52
Vijay Pushkin <vijaypushkin.a@gmail.com>
2023-06-27 23:27:34 +0530

    chore: test suite added for library

--- libraries/pushkin-simple-modals/vite.config.ts
+++ libraries/pushkin-simple-modals/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import dts from 'vite-plugin-dts';
@@ -29,5 +29,10 @@ export default defineConfig({
         },
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.test.ts',
   },
 });
